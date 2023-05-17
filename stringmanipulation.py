dc4d23197d0299439bb53c664649ed7367504f77
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-17 13:39:37 +0530

    string manipulation and concatenation

---
+++ stringmanipulation.py
@@ -0,0 +1,8 @@
+#string manipulation
+print("hello world\nhello world\nhello world")#/n means new line
+#concatenation
+print("hello"+"sneha")#it will print without spaces
+print("hello"+" "+"sneha")#it will print with space
+print("hello "+"sneha")#another syntax
+print("hello"+" sneha")#another syntax
+    # print("hello")---shows indent error 
