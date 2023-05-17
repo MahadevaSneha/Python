e1767db2e5ceaae37eda2274eafa746a4fa772da
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-17 13:38:29 +0530

    first exercise

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("First Program - python print function")
+print("It is declared like this:")
+print("print('what to print')")
+#or
+print('print("what to print")')
+#In python both single quotes(' ') and double quotes (" ") are allowed
+print('My second program')
+# print('print('what to print')')---shows syntax error:perhaps you forget comma?
