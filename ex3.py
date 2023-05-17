779fb652d686cb3d441d16547aaecdd261dfc1d6
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-17 13:41:22 +0530

    exercise for input function

---
+++ ex3.py
@@ -0,0 +1,6 @@
+#it prints hey sneha ,how are you
+#it will take input from user and concatenate with your name
+print("Hey"+" "+input("what is your name ?")+","+"how are you ?")
+#or we can written another way also
+print("Hey "+input("what is your name ?")+" , how are you ?")
+
