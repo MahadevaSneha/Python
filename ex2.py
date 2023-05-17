074a6c797b76fb2381bd16490cf7674d0e0a78e7
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-17 13:40:25 +0530

    exercise for string manipulation

---
+++ ex2.py
@@ -0,0 +1,4 @@
+print("String Manipulation Exercise")
+print("String concatenation is done with "+" sign ")
+print("eg.print('Hello'+'sneha')")
+print("New lines can be created with a backslash and n")
