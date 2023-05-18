1b5cf840a133d469b6e915092d2983c241256d09
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 11:29:14 +0530

    camel/pascal/snake cases

---
+++ variablecases.py
@@ -0,0 +1,10 @@
+#variables can be declared in 3 ways
+#cames case-all words start with captial letter expect fist word
+myVariable=10
+print(myVariable)#10
+#pascal case-all words starts with captial letter
+MyVariableName="sneha"
+print(MyVariableName)#sneha
+#snake case-all words separated with underscore(_)
+my_variable_name="surya"
+print(my_variable_name)#surya
