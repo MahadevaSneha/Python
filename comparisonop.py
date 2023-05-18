91424353e05c3ec111a18f7ca64e304e8689efef
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 21:10:09 +0530

    comparison operator

---
+++ comparisonop.py
@@ -0,0 +1,11 @@
+#Comparison operator / Relational operator
+
+a=5
+b=2
+print(a<b)#false
+print(a>b)#true
+print(a>=b)#true
+print(a<=b)#false
+print(a!=b)#true
+print((b+3)==a)#true
+print(a==b)#false
