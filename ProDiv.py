f5902a06df4e51dc1b5bf53bbc3bd7baa9cfdadd
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 23:07:55 +0530

    mul,div,modulo

---
+++ ProDiv.py
@@ -0,0 +1,8 @@
+#Write a program to get 2 numbers from the user and calculate their product, quotient and remainder using '*', '/' and '%' operators respectively. Print the corresponding product, quotient and remainder of the numbers as output in the console.
+
+
+a=int(input("Enter a value: "))#50
+b=int(input("Enter b value: "))#10
+print(a*b)#500
+print(a//b)#5
+print(a%b)#0
