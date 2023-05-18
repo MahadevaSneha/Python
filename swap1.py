b8a31529508b3e405d5d46cd77b092f36de023c6
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 23:08:54 +0530

    without using 3rd variable

---
+++ swap1.py
@@ -0,0 +1,8 @@
+# Write a program to swap two values without the use of 3rd variable
+a=int(input("Enter a value: "))#10
+b=int(input("Enter b value: "))#20
+a=a+b
+b=a-b
+a=a-b
+print(a)#20
+print(b)#10
