84d33ad4f15d09d1090a699c41d288a5797cc6ce
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 23:01:41 +0530

    average of numbers

---
+++ avg.py
@@ -0,0 +1,8 @@
+#A teacher wants to compute the average of 5 students in her class. Write a program to help her to find the average. The average is the sum of all the numbers, then divided by the total numbers.
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+sum=(a+b+c+d+e)/5
+print("{:.2f}".format(sum))
