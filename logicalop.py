e7fc93b934f18413741cb97a2a73faf37a06b155
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 21:22:19 +0530

    logical operator

---
+++ logicalop.py
@@ -0,0 +1,22 @@
+#logical operators
+# and or not
+# and logical operator
+print(5<2 and 2>5)#false
+print(10>1 and 1<10)#true
+print(10>1 and 1>10)#false
+print(1<0 and 2<0)#false
+
+# or logical operator
+
+print(5>2 or 2>5)#true
+print(10>1 or 1<10)#true
+print(10>1 or 1>10)#true
+print(1<0 or 2<0)#false
+
+#not logical operator
+
+print(not(5))#false
+a=False
+print(not(a))#true
+b=True
+print(not(b))#false
