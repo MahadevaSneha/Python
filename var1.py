13595e115bf94761970bfdc610c461b5b6484514
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 11:28:13 +0530

    variables

---
+++ var1.py
@@ -0,0 +1,9 @@
+#variables in python
+name=input("what is your name?")#assigment operator is used to assign a value to a variable 
+print(name)#displaying the stored name\
+
+a=1
+print(a)
+#values can change ,it is temporary it is stored
+a=5
+print(a)
