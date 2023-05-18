8589f96c1ac754b1058773346ca849d51dc9806b
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 21:05:06 +0530

    assignment operator in python

---
+++ assignmentop.py
@@ -0,0 +1,37 @@
+#Assignment operator in python i.e '='
+#+= ,-=,*=,/=,//=,**=,%= shorthand assignment operator
+a=5
+print(a)#5
+#multiple values
+a,b,c=1,2,3
+print(a,b,c)#1,2,3
+# += shorthand assignment operator
+d,e=4,3
+f=d+e
+d+=2
+f+=d
+print(d)#6
+print(f)#13
+# -= shorthand assignment operator
+m=2
+n=1
+m-=n
+print(m)#1
+# *= shorthand assignment operator
+m*=n
+print(m)#1
+# /=shorthand assignment operator
+m/=n
+print(m)#1.0
+# %= shorthand assignment operator
+m%=n
+print(m)#0.0
+#//= shorthand assignment operator
+d//=2
+print(d)#3
+# **= shorthand assignment operator
+d**=2
+print(d)#9
+
+
+
