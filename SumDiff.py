041581aa2675961ce5750631f93f91865a8cd5fe
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 23:08:25 +0530

    add n sub

---
+++ SumDiff.py
@@ -0,0 +1,5 @@
+#Write a program to get 2 numbers from the user and calculate their sum and difference using '+' and '-' operators respectively
+a=int(input("Enter First Number: "))#55
+b=int(input("Enter Second Number: "))#34
+print(a+b)#89
+print(a-b)#21
