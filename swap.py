1b272ff023a1a60c260552184e4dcc47b28250a6
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 11:27:50 +0530

    swapping of variables using temp variable

---
+++ swap.py
@@ -0,0 +1,11 @@
+#swapping of two variables using a temporary variable that is temp
+a=input("enter a value:")#1
+b=input("enter b value:")#2
+temp=a
+a=b
+b=temp
+
+ 
+#instead of + we can use , also because in python it accepts both to concatenate
+print("a="+a)#2
+print("b="+b)#1
