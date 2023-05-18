eec4e4d16400a70b1fb91a72b595854d8df708a8
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-18 11:28:44 +0530

    length of a string/variable

---
+++ varlen.py
@@ -0,0 +1,31 @@
+#finding the length of the string
+name=input("What is full name?")#taking input from the user
+length=len(name)#assigning len function another variable
+print("the length of your name is ",length)
+#another example for finding the length of the string
+student_name="Sneha"
+print(len(student_name))
+#another example
+first_name=input("Enter your First name:")
+second_name=input("Enter your Last name:")
+print("Your first name length is:",len(first_name))
+print("Your last name length is:",len(second_name))
+
+
+#assign string to the variables and concatenating that one
+name1="sneha"
+name2="surya"
+print(name1+name2)
+#the below example show type error
+#   n=1
+#   m=sneha
+#   print(n+m) --->shows type error because int and str cannot be concatenated
+
+#another way to concate int to string
+
+m=input("enter you fav number:")#it will take the value as string datatype
+print(name2+m)
+
+
+
+
