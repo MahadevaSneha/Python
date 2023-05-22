7f30b47586e4b17b7c762569231cb7b86fb73f1a
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-22 23:40:35 +0530

    midpoint

---
+++ three.py
@@ -0,0 +1,9 @@
+''' midpoint of given values'''
+x1=int(input())#2
+y1=int(input())#4
+x2=int(input())#10
+y2=int(input())#15
+md1=float((x1+x2)/2)#6.0
+md2=float((y1+y2)/2)#9.5
+print("Binoy's house is located at ({},{} )".format(md1,md2))
+#Binoy's house is located at (6.0,9.5)
