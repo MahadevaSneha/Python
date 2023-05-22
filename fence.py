90af89ab78d451cb30fa038f111516f8fd6f09e1
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-22 23:44:05 +0530

    fencing prblm

---
+++ fence.py
@@ -0,0 +1,7 @@
+#a program to find the exact length of the rope and the exact quantity of carper that is required
+length=int(input())#50
+breadth=int(input())#20
+length_of_rope=2*(length+breadth)
+qunatity_of_carpet=length*breadth
+print("Required length is {} m".format(length_of_rope))#140
+print("Required qunatity of carpet is {} sqm".format(qunatity_of_carpet))#1000
