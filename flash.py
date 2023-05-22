268ba28fb7dfc535fa05cbb082f2e7387afafdcf
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-22 23:40:03 +0530

    calculating midpoints

---
+++ flash.py
@@ -0,0 +1,10 @@
+''' midpoint of given values'''
+x1=int(input())#2
+y1=int(input())#4
+x2=int(input())#10
+y2=int(input())#15
+md1=float((x1+x2)/2)#6.0
+md2=float((y1+y2)/2)#9.5
+print(md1)
+print(md2)
+
