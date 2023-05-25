05efc608e91ff23aec135e86beaa134b99e77c60
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-25 23:14:31 +0530

    membership operator in python

---
+++ membership.py
@@ -0,0 +1,15 @@
+#membership Operator
+str="I'm a good girl"
+print('good' in str)#true
+print('s' in str)#false
+print('a' not in str)#false
+print('s' not in str)#true
+
+
+'''
+take list example
+'''
+l=[2,4,5,6,8,8]
+print(20 not in l)#true
+print(2 in l)#true
+print(5 not in l)#false
