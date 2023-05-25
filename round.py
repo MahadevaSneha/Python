c18003b25d8eab423830e88eaf702476bccec8b3
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-25 23:13:55 +0530

    round_function in python

---
+++ round.py
@@ -0,0 +1,50 @@
+
+#round function in python
+print(round(7))#7 --- return int
+print(round(7,2))#7 ---- return int
+print(round(7.62))# 8 ---return int
+print(round(2.6666,2))#2.67 ---return float
+print(round(2.6657,0))#3.0 ---return float
+print(round(7.5))# 8 returns nearest evne number
+print(round(11.5))#12
+print(round(12.5))#12
+print(round(672,2))#672
+print(round(672,0))#672
+print(round(674,-1))#670
+print(round(674,-2))#700
+print(round(674,-3))#1000
+print(round(674,-4))#0
+print(round(665,-1))#660
+print(round(675,-1))#680
+print(round(-8/3))#-3
+print(round(-1.5))#-2
+print(round(675,1))#675
+print(round(685,1))#685
+print(round(674.1012,-1))#670.0
+print(round(1212,-2))# 1200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
