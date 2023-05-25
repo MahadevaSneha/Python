dac1e21c2895d7a8b130a46bbfebc32b22720acf
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-25 23:13:06 +0530

    exercise 5

---
+++ ex5.py
@@ -0,0 +1,7 @@
+#coding exercise 
+age=int(input("Enter your age: "))
+years_left=90-age
+days_left=years_left*365
+months_left=years_left*12
+weeks_left=years_left*52
+print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left")
