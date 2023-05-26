299be9976473430d39a3f0742db6427e6c37bf0a
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-26 20:11:12 +0530

    coding exercise

---
+++ ex6.py
@@ -0,0 +1,6 @@
+#checking whether the given number is even or odd by using if--else statement in python
+number=int(input("Enter a number: "))#4,5
+if number%2==0:
+    print(f"given number \"{number}\" is even")
+else:
+    print(f"given number \"{number}\" is odd")
