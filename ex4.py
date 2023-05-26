07b0c33f03a1edeede0cd2931474dd64cba9a36f
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-26 20:12:20 +0530

    coding exercise

---
+++ ex4.py
@@ -0,0 +1,8 @@
+#calcuating the Body Mass Index
+'''
+Formula for BMI is weight/height*height
+'''
+weight=int(input("Enter weight in Kilo gram(KG):"))
+height=float(input("Enter height in meter(m):"))
+BMI=int(weight/(height*height))
+print(BMI)
