d49ac4a0cbf018113ce2ac858183ce313d1e78a3
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-26 20:11:48 +0530

    conditional statements in python

---
+++ cond_stmts.py
@@ -0,0 +1,9 @@
+#conditional statements/decison making statements in python
+#if ---- else in python
+height=int(input("enter height in feet: "))
+if(height>3):
+    print("Token is required")
+    print("Now you can board the metro!")
+else:
+    print("No Token is required")
+    print("Now you can board the metro!")
