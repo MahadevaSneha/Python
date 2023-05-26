b310cb4710ef5feddd41378bd13c8d9ca3bde64e
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-05-26 20:09:39 +0530

    coding exercise

---
+++ ex7.py
@@ -0,0 +1,12 @@
+#Example for if_elif_else statement
+number=int(input("Enter a number: "))
+if number==1:
+    print("One")
+elif number==2:
+    print("Two")
+elif number==3:
+    print("Three")
+elif number==4:
+    print("Four")
+else:
+    print("Wrong Input")
