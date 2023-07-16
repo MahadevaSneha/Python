ecfa981dba8179c34c43e4142400560f614f712f
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-07-16 20:29:03 +0530

    Create pattern1.py

---
+++ pattern1.py
@@ -0,0 +1,7 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and 
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print("*",end=" ")
+    print()
