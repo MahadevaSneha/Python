b26c19abe94e91695941a9b33b26372c5b4c92df
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-07-16 20:58:55 +0530

    Create pattern3.py

---
+++ pattern3.py
@@ -0,0 +1,10 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and 
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if(i==1 or i==n or j==1 or j==6 or i==j):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
