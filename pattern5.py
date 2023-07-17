5695afe4452b08f3923b489c298e3152e05f3ab1
Sneha Mahadeva <116667567+SnehaMahadeva@users.noreply.github.com>
2023-07-17 22:42:48 +0530

    Create pattern5.py

---
+++ pattern5.py
@@ -0,0 +1,8 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if(i==1 or i==n or j==1 or j==n or i==j or n+1-i==j):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
