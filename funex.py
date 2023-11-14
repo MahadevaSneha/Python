0754931b62874c33cefad7af10c34843cfd909f2
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2023-11-14 20:32:15 +0530

    Create funex.py

---
+++ funex.py
@@ -0,0 +1,12 @@
+def sqrt(num):
+    a=num*num
+    return a
+a=sqrt(int(input("Enter a number to find sqrt: ")))
+print("The sqrt is",a)
+
+
+"""
+Enter a number to find sqrt: 4
+The sqrt is 16
+
+"""
