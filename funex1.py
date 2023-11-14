1c10f0c0b5d89477ebf939739535b1137d7fec50
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2023-11-14 20:37:54 +0530

    Create funex1.py

---
+++ funex1.py
@@ -0,0 +1,13 @@
+#write a  program to accept 2 numbers frm the user as input and return it's sum
+def sum(x,y):
+    sum=x+y
+    return sum
+res=sum(int(input("Enter frst num:")),int(input("Enter scnd num:")))
+print("The sum of two numbers is ",res)
+
+
+"""
+Enter frst num:5
+Enter scnd num:79
+The sum of two numbers is  84
+"""
