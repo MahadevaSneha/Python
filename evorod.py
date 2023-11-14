2e06613d6dd50669c4218be4f6c89377621e3b4f
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2023-11-14 20:43:20 +0530

    Create evorod.py

---
+++ evorod.py
@@ -0,0 +1,17 @@
+#write a program to check whether the given number is odd or even
+def evorod(num):
+    if num%2==0:
+        print(num,"is even number")
+    else:
+        print(num,"is odd number")
+
+evorod(int(input("Enter a num to check even or odd:")))
+evorod(int(input("Enter a num to check even or odd:")))
+
+
+"""
+Enter a num to check even or odd:655
+655 is odd number
+Enter a num to check even or odd:490
+490 is even number
+"""
