0ddcb54c4211d93721b7212f0cd8b433f5ee9c5a
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2023-11-20 20:55:59 +0530

    Create lambafunex.py

---
+++ lambafunex.py
@@ -0,0 +1,13 @@
+sqnum=lambda n:n*n
+print("The square value of a given number is:",sqnum(int(input("Enter a number:"))))
+
+
+"""
+"C:\Users\Sneha M\PycharmProjects\pythonProject\ practice\venv\Scripts\python.exe" "C:\Users\Sneha M\python_class\main.py" 
+Enter a number:100
+The square value of a given number is: 10000
+
+Process finished with exit code 0
+
+
+"""
