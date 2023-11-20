c91d4dd82b544c14813b9e812123706300f4e706
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2023-11-20 21:04:49 +0530

    Create biggestvalue.py

---
+++ biggestvalue.py
@@ -0,0 +1,14 @@
+#Write a program to find biggest of given values
+
+bv=lambda n,m:n if n>m else m
+print("THe Biggest Value is: ",bv(int(input("Fst num:")),int(input("Scnd num:"))))
+
+"""
+"C:\Users\Sneha M\PycharmProjects\pythonProject\ practice\venv\Scripts\python.exe" "C:\Users\Sneha M\python_class\main.py" 
+Fst num:2
+Scnd num:1
+THe Biggest Value is:  2
+
+Process finished with exit code 0
+
+"""
