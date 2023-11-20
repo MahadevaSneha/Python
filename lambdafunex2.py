235231b7884281272166766ff19700284c471004
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2023-11-20 21:01:23 +0530

    Create lambdafunex2.py

---
+++ lambdafunex2.py
@@ -0,0 +1,15 @@
+#Write a program to find sum of two numbers using lambda function
+
+sum=lambda n,m:n+m
+print("The Sum is:",sum(int(input("Enter fisrt number: ")),int(input("Enter second number: "))))
+
+
+"""
+"C:\Users\Sneha M\PycharmProjects\pythonProject\ practice\venv\Scripts\python.exe" "C:\Users\Sneha M\python_class\main.py" 
+Enter fisrt number: 20
+Enter second number: 50
+The Sum is: 70
+
+Process finished with exit code 0
+
+"""
