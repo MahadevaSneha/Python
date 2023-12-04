d463346fe33512dc0619a75898f90b077f2be685
Mahadeva Sneha <116667567+MahadevaSneha@users.noreply.github.com>
2023-12-04 19:41:34 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,16 @@
+# To find square of given numbers
+x=eval(input())
+l=list(map(lambda m:m*m,x))
+print(l)
+
+
+"""
+"C:\Users\Sneha M\PycharmProjects\pythonProject\ practice\venv\Scripts\python.exe" "C:\Users\Sneha M\python_class\main.py" 
+[1,2,3,4,5]
+[1, 4, 9, 16, 25]
+
+Process finished with exit code 0
+
+
+
+"""
